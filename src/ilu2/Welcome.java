ea94d8d150cd4b9f1b9a2281f19e6718ed2fda5c
Robin Mounié <robin.mounie@gmail.com>
2023-04-21 14:24:44 +0200

    EX_1: Hello Bob

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,9 +1,17 @@
 package ilu2;
 
 public class Welcome {
-public Welcome() {
-}
-public String welcome(String nom) {
-	return nom;
-}
+
+	public Welcome() {
+
+	}
+	public String welcome(String nom) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello, ");
+		sb.append(Character.toUpperCase(nom.charAt(0)));
+		for(int i =1; i<nom.length();i++) {
+			sb.append(nom.charAt(i));
+		}
+		return sb.toString();
+	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -6,11 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
-
+	private Welcome w;
+	@BeforeEach
+	void setUp() throws Exception{
+		this.w = new Welcome();
+	}
 	@Test
 	void test() {
-
-		fail("Not yet implemented");
+		assertEquals("Hello, Bob", w.welcome("bob"));
+		assertEquals("Hello, Andreas", w.welcome("andreas"));
+		assertEquals("Hello, Billi", w.welcome("billi"));
+		
 	}
 
 }
